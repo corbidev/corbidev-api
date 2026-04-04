dbab1156ae9346bbb7cbef081f514f7be09f005b
CORBISIER Eric <contact@corbisier.fr>
2026-04-04 15:31:15 +0200

    Réalisation : Initialisation d'un projet d'API Symfony avec une configuration et une structure de base.
    - Ajout des fichiers de configuration de routage pour les contrôleurs, la plateforme API, la gestion des erreurs du framework et la sécurité.
    
    - Création de la configuration des services pour l'injection de dépendances.
    
    - Mise en place de la structure initiale des migrations de base de données avec `.gitignore`.
    
    - Implémentation du point d'entrée principal dans `index.php`.
    
    - Définition de l'entité `Log` et de son dépôt pour les interactions avec la base de données.
    
    - Implémentation du noyau de l'application avec `MicroKernelTrait`.
    
    - Ajout d'un modèle HTML de base pour le rendu des vues.
    
    - Configuration de Docker Compose pour les services de base de données (MariaDB et Adminer).

---
+++ api/src/Entity/Log.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\LogRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LogRepository::class)]
+class Log
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+}
