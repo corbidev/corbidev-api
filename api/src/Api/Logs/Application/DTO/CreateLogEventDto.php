822137dbd667483c1b6b647548ca47e977720fe4
Eric Corbisier <contact@corbisier.fr>
2026-04-19 23:21:44 +0200

    Modification du DTO pour activer api-platform

--- api/src/Api/Logs/Application/DTO/CreateLogEventDto.php
+++ api/src/Api/Logs/Application/DTO/CreateLogEventDto.php
@@ -2,7 +2,18 @@
 
 namespace App\Api\Logs\Application\DTO;
 
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Post;
 
+#[ApiResource(
+    operations: [
+        new Post(
+            uriTemplate: '/logs',
+            host: '%api_host%'
+        )
+    ]
+)]
 final class CreateLogEventDto
 {
     #[Assert\NotBlank]
--- api/src/Api/Logs/Domain/Entity/LogEvent.php
+++ api/src/Api/Logs/Domain/Entity/LogEvent.php
@@ -4,18 +4,6 @@ namespace App\Api\Logs\Domain\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
-use Symfony\Component\Validator\Constraints as Assert;
-use ApiPlatform\Metadata\ApiResource;
-use ApiPlatform\Metadata\Post;
-
-#[ApiResource(
-    operations: [
-        new Post(
-            uriTemplate: '/logs',
-            host: '%api_host%'
-        )
-    ]
-)]
 #[ORM\Entity]
 #[ORM\Table(name: 'CBV_LOGS_ENV')]
 class LogEnv
