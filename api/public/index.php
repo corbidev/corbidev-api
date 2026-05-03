cd8dcaefef396a90e26d893447902fad1af7c284
Eric Corbisier <contact@corbisier.fr>
2026-05-03 20:01:13 +0200

    Ajout de la gestion des fichiers et mécanique anti boucle sur les logs

--- api/public/index.php
+++ api/public/index.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Kernel;
-
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
-
+/**
+#ini_set('display_errors', '1');
+#error_reporting(E_ALL);
+*/
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return static function (array $context) {
